5594ea7c92cca07dd04a9436d74d62d0781a0ceb
angelxry <angel.xinranyu@gmail.com>
2024-03-07 11:25:51 -0500

    upd

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCZwy7AZ-bIAFmh0dx4ubWbML1SUQYXgD8",
+    authDomain: "react-notes-manager-9d771.firebaseapp.com",
+    projectId: "react-notes-manager-9d771",
+    storageBucket: "react-notes-manager-9d771.appspot.com",
+    messagingSenderId: "816993149893",
+    appId: "1:816993149893:web:78604dfcb777ed191aa1ac"
+  };
--- src/containers/NoteList/NoteList.jsx
+++ src/containers/NoteList/NoteList.jsx
@@ -1,15 +1,16 @@
 import { NoteAPI } from "api/note";
 import { TextCard } from "components/TextCard/TextCard";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { deleteNote } from "store/notes/notes-slice";
 import s from "./style.module.css";
+
 export function NoteList({ noteList }) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   async function deleteNote_(note) {
-    if (window.confirm("Delete note ?")) {
+    if (window.confirm("Delete note?")) {
       NoteAPI.deleteById(note.id);
       dispatch(deleteNote(note));
     }
